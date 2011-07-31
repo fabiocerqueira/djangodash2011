867ac975569660b663e55d7a1b80604192c6bcc5
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2011-07-31 14:44:02 -0300

    add formset Presentation of the registration of the event

--- src/pylestras/activity/forms.py
+++ src/pylestras/activity/forms.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm
+from django.forms.models import inlineformset_factory
 
-from activity.models import Event
+from activity.models import Event, Presentation
 
 class EventCreateForm(ModelForm):
     class Meta:
         model = Event
         exclude = ('admin', 'slug') 
 
+
+PresentationFormSet = inlineformset_factory(Event,
+    Presentation,
+    can_delete=False,
+    extra=3)
--- src/pylestras/activity/views.py
+++ src/pylestras/activity/views.py
@@ -3,9 +3,10 @@ from django.views.generic import ListView, DetailView, CreateView
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
+from django.template import RequestContext
 
 from activity.models import Event, Presentation
-from activity.forms import EventCreateForm
+from activity.forms import EventCreateForm, PresentationFormSet
 
 
 class EventDetailView(DetailView):
@@ -29,9 +30,26 @@ class DashboardCreateEvent(CreateView):
     template_name='activity/dashboard/event_add.html'
 
     def form_valid(self, form):
+        context = self.get_context_data()
         self.object = form.save(commit=False)
         self.object.admin = self.request.user
-        self.object.save()
-        messages.success(self.request, _('Event %s registered successfully!') % self.object.name)
-        return super(DashboardCreateEvent, self).form_valid(form)
-        
+        presentation_formset = context['presentation_formset']
+        if presentation_formset.is_valid():
+            self.object.save()
+            presentation_formset.instance = self.object
+            presentation_formset.save()
+            messages.success(self.request, _('Event %s registered successfully!') % self.object.name)
+            return super(DashboardCreateEvent, self).form_valid(form)
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
+
+    def form_invalid(self, form):
+        return self.render_to_response(self.get_context_data(form=form))
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardCreateEvent, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['presentation_formset'] = PresentationFormSet(self.request.POST)
+        else:
+            context['presentation_formset'] = PresentationFormSet()
+        return context
