58c87518c824ad03eeeee7c9b2590a8278800ac1
Mário Chaves <macndesign@gmail.com>
2011-07-31 14:46:54 -0300

    Link para o detalhamento das apresentações

--- src/pylestras/activity/urls.py
+++ src/pylestras/activity/urls.py
@@ -5,6 +5,7 @@ from activity.views import *
 
 urlpatterns = patterns('',
     url(r'^event/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$', EventDetailView.as_view(), name="activity_event"),
+    url(r'^event/presentation/(?P<pk>\d+)/$', PresentationDetailView.as_view(), name="presentation_event"),
 )
 
 #dashboard urls
