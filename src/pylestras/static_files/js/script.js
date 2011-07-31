707be3758683aa42d1de1071ab508a99faa06460
Mário Chaves <macndesign@gmail.com>
2011-07-31 12:42:24 -0300

    Script de form

--- src/pylestras/static_files/js/script.js
+++ src/pylestras/static_files/js/script.js
@@ -3,3 +3,10 @@ Fábio Cerqueira <cerqueirasfabio@gmail.com>
 Italo Maia <italo.maia@gmail.com>
 Mário Chaves <macndesign@gmail.com>
 */
+
+$(document).ready(function(){
+
+    // Foca no primeiro input de qualquer form.
+    $("form").find("input:visible:first").focus();
+    
+});
